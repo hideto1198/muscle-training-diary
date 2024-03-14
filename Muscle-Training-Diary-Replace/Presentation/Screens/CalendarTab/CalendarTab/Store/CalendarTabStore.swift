857169e4bf7fae0a876bb9691d8634934171c1c8
hideto1198 <hideto1198@yahoo.co.jp>
2024-03-14 17:10:19 +0900

    パフォーマンス改善

--- Muscle-Training-Diary-Replace/Presentation/Screens/CalendarTab/CalendarTab/Store/CalendarTabStore.swift
+++ Muscle-Training-Diary-Replace/Presentation/Screens/CalendarTab/CalendarTab/Store/CalendarTabStore.swift
@@ -10,19 +10,30 @@ import ComposableArchitecture
 
 @Reducer
 struct CalendarTabStore {
+    enum CancelId { case onPaging }
     var body: some ReducerOf<Self> {
         BindingReducer()
         Reduce { state, action in
             switch action {
+            case .binding(\.tabSelection):
+                return .concatenate(
+                    .cancel(id: CancelId.onPaging),
+                    .run { send in
+                        await send(.view(.onCenter))
+                    }.cancellable(id: CancelId.onPaging)
+                )
             case .view(.onCenter):
                 let currentState = state.calendarStates.first { $0.id == state.tabSelection }!
-                guard state.calendarStates[1] != currentState else { return .none }
-                let calendarStates = [
-                    CalendarStore.State(year: currentState.backward.0, month: currentState.backward.1),
-                    currentState,
-                    CalendarStore.State(year: currentState.forward.0, month: currentState.forward.1)
-                ]
-                state.calendarStates = calendarStates
+                guard let currentIndex = state.calendarStates.firstIndex(where: { $0.id == state.tabSelection }),
+                      currentIndex != 1
+                else { return .none }
+                if currentIndex == state.calendarStates.count - 1 {
+                    state.calendarStates.append(CalendarStore.State(year: currentState.forward.0, month: currentState.forward.1))
+                    state.calendarStates.removeFirst()
+                } else if currentIndex == 0 {
+                    state.calendarStates.insert(CalendarStore.State(year: currentState.backward.0, month: currentState.backward.1), at: 0)
+                    state.calendarStates.removeLast()
+                }
                 return .none
             default:
                 return .none
